1afb2a486a30d326f6c9d6c20d8a711a8cc0db3d
konishis <gts.0424.koni2@gmail.com>
2021-02-09 10:42:15 +0900

    b

--- PEQ1.py
+++ PEQ1.py
@@ -1,11 +1,11 @@
-'''
+"""
 3 または 5 の倍数である 10 未満のすべての自然な数値をリストすると、3、5、6、9 が得られます。これらの倍数の合計は 23 です。
 
 1000 以下の 3 または 5 のすべての倍数の合計を検索します。
-'''
+"""
 
 def calnumset(numrange, versusnum):
-    numset = {num for num in range(1,numrange) if num % versusnum == 0}
+    numset = {num for num in range(1, numrange) if num % versusnum == 0}
     return numset
 
 
