e09bf11a104e678bfb2993abc1d5f859d6ac96b4
konishis <gts.0424.koni2@gmail.com>
2021-02-09 10:59:05 +0900

    test ans 2

--- tests/test_PEQ1.py
+++ tests/test_PEQ1.py
@@ -1,6 +1,10 @@
 import peq1
 
 
-def test_peq1(capsys):
-    assert peq1.aaa(3, 5, 10) == 23
+def test_peq1():
+    result = peq1.aaa(3, 5, 10)
+    assert result == 23
     
+def test_ans():
+    result = peq1.aaa(3, 5, 1000)
+    assert result == 233168
